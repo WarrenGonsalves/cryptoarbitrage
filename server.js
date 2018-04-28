ce2aff837893118544fb76cdccf2f624b979e60e
Sobin George Thomas <sobingt@gmail.com>
2018-04-28 13:35:39 +0530

    Added bittrex modules

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ var CronJob = require('cron').CronJob;
 var request = require('request');
 var rp = require('request-promise');
 var Promise = require("bluebird");
+var currency = 'ETH';
+var coin = 'ZEC'
+
+const bittrex = require('./node.bittrex.api');
+const APIKEY = process.env.BITTREXKEY;
+const APISECRET = process.env.BITTREXSECRET;
+let totalXRPQuantity = 53;
+bittrex.options({ 
+  'apikey' : APIKEY, 
+  'apisecret' : APISECRET, 
+  'stream' : false, 
+  'verbose' : false, 
+  'cleartext' : false 
+});
 
 new CronJob('*/5 * * * * *', function() {
   console.log('You will see this message 5 second');
@@ -18,27 +32,22 @@ function compareRates(a, b, c, d) {
 	Promise.all([a, b, c, d])
 	.then(function(data) {  
 
-  	console.log("bittrex")
-  	console.log(data[0].result.sell[0])
+  	console.log("Bittrex OrderBook ", data[0].result.sell[0])
     // console.log(data[0].result.sell[1])
     // console.log(data[0].result.sell[2])
 
-		console.log("hitbtc")
-    console.log(data[1].bid[0])
+		console.log("Hitbtc OrderBook ", data[1].bid[0])
     // console.log(data[1].bid[1])
     // console.log(data[1].bid[2])
 
 		console.log("---- Last Trade ----")
-		console.log("bittrex")
+		console.log("bittrex ",data[2].result[0].Price, data[2].result[0].OrderType)
     // console.log(data[2])
-    console.log(data[2].result[0].Price, data[2].result[0].OrderType)
+		console.log("hitbtc ",data[3][0].price, data[3][0].side)
 
-		console.log("hitbtc")
-    console.log(data[3][0].price, data[3][0].side)
-
-    // console.log( parseFloat(data[1].bid[0].price), data[0].result.sell[0].Rate );
+		// console.log( parseFloat(data[1].bid[0].price), data[0].result.sell[0].Rate );
 		console.log("---- Arbit ----")
-    let arbit = [( parseFloat(data[3][0].price) / data[2].result[0].Price ) * 100 - 100];
+    let arbit = [( parseFloat(data[1].bid[0].price) / data[0].result.sell[0].Rate ) * 100 - 100];
     console.log(arbit)
 	})
 	.catch(function (err) {
@@ -49,7 +58,7 @@ function compareRates(a, b, c, d) {
 function getBittrixOrderBook() {
 	var options = { method: 'GET',
 	  url: 'https://bittrex.com/api/v1.1/public/getorderbook',
-	  qs: { type: 'both', market: 'ETH-XRP' },
+	  qs: { type: 'both', market: `${currency}-${coin}` },
 	  json: true
 	};
 
@@ -60,7 +69,7 @@ function getBittrixOrderBook() {
 
 function getHitbtcOrderBook() {
 	var options = { method: 'GET',
-	  url: 'https://api.hitbtc.com/api/2/public/orderbook/XRPETH',
+	  url: `https://api.hitbtc.com/api/2/public/orderbook/${coin}${currency}`,
 	  json: true
 	};
 
@@ -71,7 +80,7 @@ function getHitbtcOrderBook() {
 function getBittrixLastTrade() {
 	var options = { method: 'GET',
 	  url: 'https://bittrex.com/api/v1.1/public/getmarkethistory',
-	  qs: { market: 'ETH-XRP' },
+	  qs: { market: `${currency}-${coin}` },
 	  json: true
 	};
 
@@ -82,10 +91,79 @@ function getBittrixLastTrade() {
 
 function getHitbtcLastTrade() {
 	var options = { method: 'GET',
-	  url: 'https://api.hitbtc.com/api/2/public/trades/XRPETH?sort=DESC',
+	  url: `https://api.hitbtc.com/api/2/public/trades/${coin}${currency}?sort=DESC`,
 	  json: true
 	};
 
 	return rp(options)
 
 }
+
+
+
+
+//buyETHXRP(rate)
+function buyETHXRP(rate, quantity = 53){
+  totalXRPQuantity -= quantity
+  bittrex.buylimit({ market : 'ETH-XRP',  
+                    quantity: quantity, 
+                    rate: rate
+                  }, 
+  function( data, err ) {
+    console.log( data );
+    console.log( err );
+  });
+
+}
+
+
+/**
+ *  sendCustomRequest example
+ */
+// bittrex.sendCustomRequest( 'https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-ltc', function( data ) {
+//   console.log( data );
+// }, true);
+
+// bittrex.getbalance({ currency : 'ETH' }, function( data, err ) {
+//   console.log( data );
+//   console.log( err );
+// });
+
+// bittrex.getbalances( function( data, err ) {
+//   console.log( data );
+//   console.log( err );
+// });
+
+// bittrex.buylimit({ market : 'BTC-ETH',  
+//                    quantity: 0.14802553, 
+//                    rate: 0.07300400}, function( data, err ) {
+//   console.log( data );
+//   console.log( err );
+// });
+
+
+bittrex.getopenorders({}, function( data, err ) {
+  console.log( data );
+  console.log( err );
+});
+
+// bittrex.cancel({uuid:'4a9f577e-d8b0-44e4-836e-8406ec1e5bf4'}, function( data, err ) {
+//   console.log( data );
+//   console.log( err );
+// });
+
+// /**
+//  *  getmarkethistory example
+//  */
+// bittrex.getmarkethistory( { market : 'BTC-LTC' }, function( data ) {
+//   console.log( data.result );
+// });
+
+// /**
+//  *  getorderbook example
+//  */
+// bittrex.getorderbook( { market : 'BTC-PIVX', depth : 10, type : 'both' }, function( data ) {
+
+//     data.result.buy.forEach(function(dataset) { console.log(dataset); });
+//     data.result.sell.forEach(function(dataset) { console.log(dataset); });
+// });
